bd6e6f41cf5e055d8ac18bed8a45eb32df25770f
GARDAR <81230062+GAR-DAR@users.noreply.github.com>
2025-02-05 12:15:45 +0200

    features/signalR_package_&_realtime_message_&_user_online_check

---
+++ Backend/Backend/Hubs/ChatHub.cs
@@ -0,0 +1,39 @@
+﻿using Backend.Models.ModelsID;
+using Backend.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Backend.Hubs
+{
+    public sealed class ChatHub : Hub
+    {
+       
+        /* debug data for postman (won't be used in gossip)
+         * 
+         *          wss://localhost:7062/chat
+         *          {"protocol":"json","version":1}
+         */
+        public override async Task OnConnectedAsync()
+        {
+            MessageModelID message = new MessageModelID(0, 0, 0, "Anonimous user connected", DateTime.Now, false, false);
+
+            await Clients.All.SendAsync("UserConnected", "Anonimous user connected");
+        }
+
+
+
+
+
+        //to send real-timemessage user must be online
+        public async Task OnConnnectedAsync(UserModelID user)
+        {
+            await Clients.All.SendAsync("UserConnected", $"{user.Username} is now online");
+        }
+
+        public async Task SendMessage(MessageModelID message)
+        {
+            MessagesService.Add(message, Backend.Program.Globals.db.Connection);
+
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,6 +1,8 @@
 
 using Backend.Services;
+using Backend.Hubs;
 using System;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Backend {
 
@@ -9,6 +11,7 @@ namespace Backend {
         public static class Globals
         {
             public static DatabaseService db = new DatabaseService();
+            //public static IHubContext<ChatHub> Clients { get; set; }
         }
 
         static void Main(string[] args)
@@ -19,7 +22,7 @@ namespace Backend {
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddSignalR();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -50,6 +53,8 @@ namespace Backend {
             app.UseCors();
 
             app.UseAuthorization();
+
+            app.MapHub<ChatHub>("/chat");
 
             app.MapControllers();
 
