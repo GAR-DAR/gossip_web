975a4f5aa31abe22674a2f09b301437f3875f5b5
OleksaLv <Oleksii.Mahinskyi.PZ.2023@lpnu.ua>
2025-02-07 10:40:29 +0200

    fixes/messages-controller-and-topics-controller

--- Backend/Backend/Controllers/MessagesController.cs
+++ Backend/Backend/Controllers/MessagesController.cs
@@ -8,7 +8,7 @@ namespace Backend.Controllers
     [Route("Messages")]
     public class MessagesController : Controller
     {
-        [HttpPost("send")] //gets MessageModelID from client, writes to db and sends it back with correct message ID given by db.
+        /*[HttpPost("send")] //gets MessageModelID from client, writes to db and sends it back with correct message ID given by db.
         //Shold delete and use ChatHub?
         public IActionResult Send([FromBody] MessageModelID message)
         {
@@ -21,15 +21,16 @@ namespace Backend.Controllers
                 return StatusCode(500, "We encountered an issue while sending your message. Please try again later.");
             }
             return Ok(dbMessage);
-        }
+        }*/
 
-        [HttpGet("all")] //gets all messages of client from db and sends them as List<MessageModelID> back.
-        //Maybe we need signal(db service) to get latest message from every client's chat from db to display them for each chat window.
-        //And signal(db service) to get all messages of chat by chat's ID. How to send user ID or selected chat ID, is it get or post method?
-        public IActionResult GetAll()
-        {
-            List<MessageModelID> messages = MessagesService.SelectMessageModelsByUserId(0, Backend.Program.Globals.db.Connection);
-            return Ok(messages);
-        }
+        /*        [HttpGet("all")] //gets all messages of client from db and sends them as List<MessageModelID> back.
+                //Maybe we need signal(db service) to get latest message from every client's chat from db to display them for each chat window.
+                //And signal(db service) to get all messages of chat by chat's ID. How to send user ID or selected chat ID, is it get or post method?
+                public IActionResult GetAll(uint userID)
+                {
+                    if (userID == 0) return BadRequest("User ID cannot be null. Please provide valid user ID.");
+                    List<MessageModelID> messages = MessagesService.SelectMessageModelsByUserId(userID, Backend.Program.Globals.db.Connection);
+                    return Ok(messages);
+                }*/
     }
 }
--- Backend/Backend/Controllers/TopicController.cs
+++ Backend/Backend/Controllers/TopicController.cs
@@ -12,10 +12,12 @@ namespace Backend.Controllers
         [HttpGet("home")]
         public IActionResult LoadAllTopics(int? page, int? amount)
         {
+            if (page < 1 || amount < 1) return BadRequest("Page number or amount of pages cannot be null. Please provide valid information.");
+
             IEnumerable<TopicModelID> topicModelIDs = (page.HasValue && amount.HasValue)
                 ? TopicsService.SelectPage(page.Value, amount.Value, Program.Globals.db.Connection)
                 : TopicsService.SelectAll(Backend.Program.Globals.db.Connection);
-          
+
             return Ok(topicModelIDs);
         }
     }
